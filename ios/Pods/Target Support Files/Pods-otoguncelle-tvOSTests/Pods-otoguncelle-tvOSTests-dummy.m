ac660fd7af416f0acac7a04f0e3e57ad23f5b5a2
nzrylmz <nzrylmz@yandex.com>
2019-08-20 18:47:01 +0300

    ilk commit

--- ios/Pods/Target Support Files/Pods-otoguncelle-tvOSTests/Pods-otoguncelle-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_otoguncelle_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_otoguncelle_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-otoguncelle/Pods-otoguncelle-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_otoguncelle : NSObject
-@end
-@implementation PodsDummy_Pods_otoguncelle
-@end
